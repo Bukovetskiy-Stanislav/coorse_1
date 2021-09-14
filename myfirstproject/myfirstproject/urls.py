e9eb33b5f9f0590e37813c7c43753910b9c5550e
Bukovetskiy-Stanislav <sstas347@gmail.com>
2021-09-14 18:24:02 +0300

    add reversed page

--- myfirstproject/myfirstproject/urls.py
+++ myfirstproject/myfirstproject/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from.import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('about/', views.about),
+    # path('admin/', admin.site.urls),
+    # path('about/', views.about),
     path('', views.home),
+    path('reverse/', views.reverse, name= "reverse"),
+
 ]
