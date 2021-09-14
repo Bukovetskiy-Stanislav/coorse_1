b863e923ada7566564f99bef4a6f4d9537c3d3da
Bukovetskiy-Stanislav <sstas347@gmail.com>
2021-09-14 18:55:25 +0300

    first project complete
    first project complete

--- myfirstproject/myfirstproject/views.py
+++ myfirstproject/myfirstproject/views.py
@@ -11,5 +11,7 @@ def home(request):
 
 def reverse(request):
     user_text = request.GET ['usertext']
+    sum_text= user_text.split()
+    suma_text= len(sum_text)
     revers_text= user_text[ ::-1]
-    return render (request, 'reverse.html', {"usertext":user_text, 'reversedtext':revers_text})
+    return render (request, 'reverse.html', {"usertext":user_text, 'reversedtext':revers_text, "sumtext":sum_text,"sumatext":suma_text})
